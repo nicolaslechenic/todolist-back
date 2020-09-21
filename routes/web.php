a581862721e126e2dca864e6c60976fe341ea384
Nicolas LE CHENIC <nicolaslechenic@gmail.com>
2020-09-21 22:55:22 +0200

    add list migration

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ $router->get('/', function () use ($router) {
 $router->get('/greetings', function () use ($router) {
     return response()->json(['message' => 'Hello world']);
 });
+
+
+$router->get('/todolists', 'ListController@index');
